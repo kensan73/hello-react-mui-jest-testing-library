0d2b60fe43cac8bdb7aaf14bb4de184903778d1d
Ken Ore <ken_ore@hotmail.com>
2025-03-19 23:16:04 -0500

    First pass at an idea.

--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { Field, Form, Formik, FormikHelpers, FormikValues } from "formik";
 import { TabContext, TabPanel, ToggleButton, ToggleButtonGroup } from "@material-ui/lab";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Thingy } from "./Thingy";
 
 export type ExperienceInfo = {
     id: string;
@@ -75,49 +76,8 @@ export const MyForm: React.FC<OwnProps> = ({ initialTableSelection, effectiveHou
                 return <Form>
                     <Typography>hi world</Typography>
                     {optimizationToggleButtons}
-                    <TabContext value={values.selectedTab}>
-                        <Tabs
-                            value={values.selectedTab}
-                            onChange={handleTabChange}
-                            indicatorColor="primary"
-                            textColor="primary"
-                            centered
-                        >
-                            <Tab label="by experience" value="by experience" />
-                            <Tab label="by room" value="by room" />
-                        </Tabs>
-                        <TabPanel value="by experience">
-                            {experienceAccordions.map((experienceAccordion, index) => (
-                                <Accordion key={`experience-accordion-${index}`}>
-                                    <AccordionSummary
-                                        expandIcon={<ExpandMoreIcon />}
-                                        aria-label="Expand"
-                                        aria-controls="additional-actions1-content"
-                                        id="additional-actions1-header"
-                                    >
-                                        <FormControlLabel
-                                            aria-label={experienceAccordion.name}
-                                            onClick={(event) => event.stopPropagation()}
-                                            onFocus={(event) => event.stopPropagation()}
-                                            control={<Checkbox />}
-                                            label={experienceAccordion.name}
-                                        />
-                                    </AccordionSummary>
-                                    <AccordionDetails>
-                                        <Typography color="textSecondary">
-                                            INSERT TABLES AND CHECKBOXES HERE
-                                        </Typography>
-                                    </AccordionDetails>
-                                </Accordion>
-                            ))}
-                        </TabPanel>
-                        <TabPanel value="by room">By room</TabPanel>
-                    </TabContext>
-                    {values.tablesForSelection.map((tableInfo) => (
-                        <div key={`checkbox-${tableInfo.id}`}>
-                            <Field type="checkbox" name="selectedTables" value={`${tableInfo.id}`} id={`${tableInfo.id}`} />
-                            <label htmlFor={`${tableInfo.id}`}>{tableInfo.name}</label>
-                        </div>))}
+
+                    <Thingy />
                 </Form>
             }}
         </Formik>);
